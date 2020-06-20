d5a1837de993634f8f35d3fe6d0fb13bee16c8e0
danyweis <danyweismail@gmail.com>
2020-06-20 20:03:30 +0100

    created the README and finished the project in adding the people oin the station

---
+++ README.md
@@ -0,0 +1,6 @@
+# ISS position
+
+In this project I use the [Open Notify](http://open-notify.org/) API to get the position of the ISS.  
+I use a map from [Free wor maps](http://www.free-world-maps.com/).  
+I this project I fetch the longitude and the latitude of the position and transfer the position on a map, and I get the names of the people currently in the space station ISS.
+![Screenshot of the project](Screenshot.png)
--- app.js
+++ app.js
@@ -10,26 +10,49 @@ function init() {
 
         setTimeout(fetchData, 5000)
     }
-    fetchData()
-    let long = ''
-    // -180west +180est
-
-    let lat = ''
-    // -90sud +90nord
 
     function getData(data) {
         let issPosition = document.querySelector('.iss')
-        long = data.iss_position.longitude
-        lat = data.iss_position.latitude
-
+        let longitudeText = document.querySelector('.longitude')
+        let latitudeText = document.querySelector('.latitude')
+        //-180 west / +180 east
+        let long = data.iss_position.longitude
+        // -90 south / +90 north
+        let lat = data.iss_position.latitude
+        longitudeText.textContent = long
+        latitudeText.textContent = lat
         let longFragment = 50 / 180
         let latFragment = 50 / 90
         let longPos = long * longFragment
         let latPos = lat * latFragment
 
-        console.log(long * longFragment)
+        //  console.log(long * longFragment)
         issPosition.style.left = `calc(${longPos}% + 50%)`
         issPosition.style.bottom = `calc(${latPos}% + 50%)`
     }
+
+    function fetchPeople() {
+        let urlPeople = 'http://api.open-notify.org/astros.json'
+        fetch(urlPeople)
+            .then((resp) => resp.json())
+            .then((data) => writePeople(data))
+            .catch((error) => console.error(error))
+    }
+
+    function writePeople(data) {
+        let people = document.querySelector('.people')
+        people.textContent = data.number
+        console.log()
+        let list = document.querySelector('ul')
+        data.people.map((a) => {
+            let name = document.createElement('li')
+            let nameText = document.createTextNode(a.name)
+            name.appendChild(nameText)
+            list.appendChild(name)
+        })
+    }
+    // CALL THE FETCHES
+    fetchData()
+    fetchPeople()
 }
 window.addEventListener('load', init)
